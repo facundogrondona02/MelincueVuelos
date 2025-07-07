d9c54657d04da3080eaa4fae0236c0a0dae40053
Facundo Grondona <facundogrondona02@gmail.com>
2025-07-07 18:09:27 -0300

    FEAT( ALL ): Modifique la varaibla de mensaje en page

--- vuelosIA/api-js/IA/IAVuelo.js
+++ vuelosIA/api-js/IA/IAVuelo.js
@@ -4,36 +4,36 @@ export async function generarJsonDesdeMensaje(mensaje) {
   return new Promise((resolve, reject) => {
     console.log("ESTO LO MUESTRA?????")
     const process = spawn("python", ["./IA/IAVuelo.py", mensaje]);
-    console.log("ESTO NO CREOOOO?????")
+   console.log("ESTO NO CREOOOO?????")
     let result = "";
     process.stdout.on("data", (data) => {
       result += data.toString();
     });
-    console.log("ESTO NO RESULT?????", result)
+   console.log("ESTO NO RESULT?????", result)
 
     process.stderr.on("data", (data) => {
       console.error("Error en Python:", data.toString());
     });
-    console.log("ESTO NO process?????", process)
+   console.log("ESTO NO process?????", process)
 
-    process.on("close", (code) => {
-      if (code === 0) {
-        try {
-          // Extraer el JSON entre llaves (primer bloque válido)
-          const jsonMatch = result.match(/\{[\s\S]*\}$/);
-          if (!jsonMatch) throw new Error("No se encontró JSON válido en la salida");
+process.on("close", (code) => {
+  if (code === 0) {
+    try {
+      // Extraer el JSON entre llaves (primer bloque válido)
+      const jsonMatch = result.match(/\{[\s\S]*\}$/);
+      if (!jsonMatch) throw new Error("No se encontró JSON válido en la salida");
 
-          const jsonString = jsonMatch[0];
-          const json = JSON.parse(jsonString);
-          console.log("JSON parseado correctamente:", json);
-          resolve(json);
-        } catch (e) {
-          console.warn("No se pudo parsear JSON, se devuelve texto plano.", e);
-          resolve(result);
-        }
-      } else {
-        reject("El script de Python falló");
-      }
-    });
+      const jsonString = jsonMatch[0];
+      const json = JSON.parse(jsonString);
+      console.log("JSON parseado correctamente:", json);
+      resolve(json);
+    } catch (e) {
+      console.warn("No se pudo parsear JSON, se devuelve texto plano.", e);
+      resolve(result);
+    }
+  } else {
+    reject("El script de Python falló");
+  }
+});
   });
 }
