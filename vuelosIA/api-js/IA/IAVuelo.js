35fd5c38d8d4e2d85ac0daeecaa9336171be2751
Facundo Grondona <facundogrondona02@gmail.com>
2025-07-07 17:32:48 -0300

    FEAT( ALL ): Modifique la varaibla de mensaje en page

--- vuelosIA/api-js/IA/IAVuelo.js
+++ vuelosIA/api-js/IA/IAVuelo.js
@@ -9,15 +9,19 @@ export async function generarJsonDesdeMensaje(mensaje) {
     process.stdout.on("data", (data) => {
       result += data.toString();
     });
+   console.log("ESTO NO RESULT?????", result)
 
     process.stderr.on("data", (data) => {
       console.error("Error en Python:", data.toString());
     });
+   console.log("ESTO NO process?????", process)
 
     process.on("close", (code) => {
       if (code === 0) {
         try {
           const json = JSON.parse(result);
+             console.log("ESTO NO prjsonocess?????", json)
+
           resolve(json);
         } catch (e) {
           console.warn("No se pudo parsear JSON, se devuelve texto plano.", e);
